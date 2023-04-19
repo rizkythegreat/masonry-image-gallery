3a5fa5d620faeeac22a794c35e85badb32bc93ae
Rizky Rahman Salam <clashikik@gmail.com>
2023-04-19 15:12:22 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import MasonryGallery from "./components/gallery/MasonryGallery";
+
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <MasonryGallery />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/gallery/MasonryGallery.jsx
@@ -0,0 +1,163 @@
+import { useState } from "react";
+
+import { classNames } from "../../utils/classNames";
+import image1 from "../../assets/architecture-1.jpeg";
+import image2 from "../../assets/landscape-3.jpeg";
+import image3 from "../../assets/neon-3.jpeg";
+import image4 from "../../assets/architecture-4.jpeg";
+import image5 from "../../assets/neon-2.jpeg";
+import image6 from "../../assets/landscape-1.jpeg";
+import image7 from "../../assets/neon-4.jpeg";
+import image8 from "../../assets/architecture-2.jpeg";
+import image9 from "../../assets/neon-1.jpeg";
+import image10 from "../../assets/architecture-3.jpeg";
+import image11 from "../../assets/landscape-2.jpeg";
+
+import "./MasonryGallery.css";
+
+const images = [
+  {
+    src: image1,
+    alt: "Metal pattern on side of a building",
+    ratio: "landscape",
+    active: 1,
+  },
+  {
+    src: image2,
+    alt: "Valley between mountains during fall",
+    ratio: "square",
+    active: 3,
+  },
+  {
+    src: image3,
+    alt: "Gas station at night lit by neon sign",
+    ratio: "portrait",
+    active: 2,
+  },
+  {
+    src: image4,
+    alt: "Hall inside building featuring a tree",
+    ratio: "square",
+    active: 1,
+  },
+  {
+    src: image5,
+    alt: "Foggy night with hotel neon sign in background",
+    ratio: "portrait",
+    active: 2,
+  },
+  {
+    src: image6,
+    alt: "Lake with mountains in the background",
+    ratio: "landscape",
+    active: 3,
+  },
+  {
+    src: image7,
+    alt: "City sidewalk on a snowy night",
+    ratio: "portrait",
+    active: 2,
+  },
+  {
+    src: image8,
+    alt: "Person going down stairs in building atrium",
+    ratio: "landscape",
+    active: 1,
+  },
+  {
+    src: image9,
+    alt: "People walking in NYC at night",
+    ratio: "landscape",
+    active: 2,
+  },
+  {
+    src: image10,
+    alt: "Dark stairs with blue lights",
+    ratio: "portrait",
+    active: 1,
+  },
+  {
+    src: image11,
+    alt: "Side of mountain with sparse trees",
+    ratio: "landscape",
+    active: 3,
+  },
+];
+
+export const MasonryGallery = () => {
+  const [activeId, setActiveId] = useState(0);
+
+  return (
+    <section className="masonry-container">
+      <header className="masonry-header">
+        <div className="masonry-info">
+          <h2 className="masonry-title">Latest works</h2>
+          <p className="masonry-subtitle">
+            Sed nec sit malesuada cursus dolor accumsan massa semper enim.
+            Egestas vel ac consectetur enim.
+          </p>
+        </div>
+        <nav className="masonry-filter">
+          <button
+            type="button"
+            onClick={() => setActiveId(0)}
+            className={
+              activeId === 0
+                ? "masonry-filter-active"
+                : "masonry-filter-inactive"
+            }
+          >
+            All
+          </button>
+          <button
+            type="button"
+            onClick={() => setActiveId(1)}
+            className={
+              activeId === 1
+                ? "masonry-filter-active"
+                : "masonry-filter-inactive"
+            }
+          >
+            Architecture
+          </button>
+          <button
+            type="button"
+            onClick={() => setActiveId(2)}
+            className={
+              activeId === 2
+                ? "masonry-filter-active"
+                : "masonry-filter-inactive"
+            }
+          >
+            Neon
+          </button>
+          <button
+            type="button"
+            onClick={() => setActiveId(3)}
+            className={
+              activeId === 3
+                ? "masonry-filter-active"
+                : "masonry-filter-inactive"
+            }
+          >
+            Landscape
+          </button>
+        </nav>
+      </header>
+      <div className="masonry-gallery">
+        {images.map(({ ratio, src, alt, active }) => (
+          <figure
+            className={classNames(
+              activeId === 0 || activeId === active
+                ? "masonry-gallery-visible"
+                : "masonry-gallery-hidden",
+              `masonry-${ratio}`
+            )}
+          >
+            <img src={src} alt={alt} />
+          </figure>
+        ))}
+      </div>
+    </section>
+  );
+};
